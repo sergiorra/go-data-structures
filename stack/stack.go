3887dc9dcab9541e2b6251d5da017d432569def9
Sergi Orra <sergiorra17@gmail.com>
2022-12-06 13:05:12 +0100

    basic implementation of stack structure

---
+++ stack/stack.go
@@ -0,0 +1,30 @@
+package stack
+
+type Stack struct {
+	values []int
+}
+
+// NewStack returns a new pointer instance of Stack
+func NewStack() *Stack {
+	return &Stack{
+		values: make([]int, 0),
+	}
+}
+
+// Push adds an element to the top of the stack
+func (s *Stack) Push(n int) {
+	s.values = append(s.values, n)
+}
+
+// Pop removes an element from the top of the stack
+func (s *Stack) Pop() int {
+	res := s.values[len(s.values)-1]
+	s.values = s.values[:len(s.values)-1]
+
+	return res
+}
+
+// IsEmpty returns true if the stack is empty
+func (s *Stack) IsEmpty() bool {
+	return len(s.values) == 0
+}
